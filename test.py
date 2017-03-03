adce0bcde58f964ed2ff16685cbb8935a89b0821
cisba <emanuele.cisbani@gmail.com>
2017-03-03 16:54:01 +0100

    ok fino alla submitOOB

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 
-import sys, yaml
+import sys, yaml, time
 from pprint import pprint
 import time4mind
 
 def test_credenziali(l):
     for i in l: 
-        pprint("--- " + i + " ---")
+        pprint('--- ' + i + ' ---')
         cred = time4user.getMobileActiveCredentials(i)
         for c in cred:
             pprint(c)
@@ -13,23 +13,22 @@ def test_credenziali(l):
             #pprint(tokenInfo)
 
 def authorize(user):
-    title = "Welcome to Cloud Signature!"
-    sender = "@CloudSignature_Bot (Telegram Bot)"
-    message = "Telegram user with nick name <b>"+user['nick']+"</b> and phone number <b>"+user['mobile']+"</b> request to use @CloudSignature_Bot with your Valid account <b>"+user['account']+"</b>."
+    title = 'Welcome to Cloud Signature!'
+    sender = '@CloudSignature_Bot (Telegram Bot)'
+    message = 'Telegram user with nick name <b>'+user['nick']+'</b> and phone number <b>'+user['mobile']+'</b> request to use @CloudSignature_Bot with your Valid account <b>'+user['account']+'</b>.'
     r = time4id.authorizeMobile(user['cred']['otpId'],user['cred']['otpProvider'],
         title,sender,message)
     return r
 
 def sign(user,docs):
-        title = "Signature Request"
-        sender = "@CloudSignature_Bot"
-        lista=""
+        title = 'Signature Request'
+        sender = '@CloudSignature_Bot'
+        lista=''
         for d in docs:
-            lista +="<li><a href='"+docs[d]+"'>"+d+"</a></li>"
-        message = "Documents to sign:"+lista
-        restHook = "http://www.tttt.it"
-        r = time4id.signMobile(user['cred']['otpId'],user['cred']['otpProvider'],title,sender,message,
-            cred['label'],restHook=restHook)
+            lista +='<li><a href=\"'+docs[d]+'\">'+d+'</a></li>'
+        message = 'Documents to sign:'+lista
+        return time4id.signMobile(user['cred']['otpId'],user['cred']['otpProvider'],title,sender,message,
+            user['cred']['label'])
 
 # Main
 
@@ -39,19 +38,18 @@ time4user = time4mind.Time4UserRPC(cfg['time4user'])
 time4id = time4mind.Time4IdRPC(cfg['time4id'])
 
 # retrive user credential
-user = { 'account': "ecisbani@intesigroup.com", 'nick': "@cisba", 'mobile': "3485115925", 'cred': None }
-cred = time4user.getMobileActiveCredentials(user['account'])
-pprint(cred)
-if not cred[0]:
-    pprint("no credential suitable found")
+user = { 'account': 'ecisbani@intesigroup.com', 'nick': '@cisba', 'mobile': '3485115925', 'cred': None }
+c = time4user.getMobileActiveCredentials(user['account'])
+pprint(c)
+if not c or not c[0]:
+    pprint('no credential suitable found')
     sys.exit(1)
 else:
-    user['cred']=cred[0]
+    user['cred']=c[0]
 pprint(user)
 
 #handler = authorize(user)
 docs={'documento_1.pdf': 'http://tttt.it/documento_1.pdf'}
-handler = sign(cred,docs)
+handler = sign(user,docs)
 pprint(handler)
-
 
--- time4mind.py
+++ time4mind.py
@@ -1,9 +1,10 @@
 
-import requests
+import requests,json
 from pprint import pprint 
 
 class Time4MindRPC:
     'Common base class for all Time4Mind RPC call'
+
     def __init__(self, conf):
         self.endpoint = conf['endpoint']
         self.cert = (conf['cert'], conf['key'])
@@ -25,68 +26,75 @@ class Time4MindRPC:
     def call(self, method, params=None):
         self.payload['method'] = method
         if params: self.payload['params'] = params
-        resp = requests.post(self.endpoint, cert=self.cert, json=self.payload, headers=self.headers)
+        j_payload = self.payload
+        #j_payload = json.dumps(self.payload)
+        print(j_payload)
+        resp = requests.post(self.endpoint, cert=self.cert, json=j_payload, headers=self.headers)
+        pprint(resp.content)
         if resp.status_code != 200: resp.raise_for_status()
         try:    
             return resp.json()['result']
         except:
-            print("Got 200.. but not result!")
+            print('Got 200.. but not result!')
             return None
 
 class Time4UserRPC(Time4MindRPC):
     'class for all Time4User RPC call'
+
     def getCredentials(self,user):
-        method = "listCredentials"
-        params = {"account": user, "credentialType": 1 }
+        method = 'listCredentials'
+        params = {'account': user, 'credentialType': 1 }
         return self.call(method, params)
+
     def getMobileActiveCredentials(self,user):
         credentials = self.getCredentials(user)
         result = []
-        # Now clean the list
-        for todo_item in credentials['credentialsData']:
-            # remove status not ACTIVE
-            if todo_item['status'] != "ACTIVE" : continue
-            # remove not having any OTP
-            if todo_item['otpDetails'] == None : continue
-            # remove OTP not on app Time4ID 
-            if todo_item['otpDetails']['otpMode'] != 1 : continue
-            # now concatenate only relevant elements
-            c = dict((k, todo_item[k]) for k in ('alias','domain','label'))
-            c['otpId'] = todo_item['otpDetails']['otpId'] 
-            c['otpProvider'] = todo_item['otpDetails']['otpProvider'] 
-            # 0=sms, 1=app, 2=email
-            c['otpMode'] = todo_item['otpDetails']['otpMode'] 
-            result += [c]
+        if credentials:
+            # Now clean the list
+            for todo_item in credentials['credentialsData']:
+                # remove status not ACTIVE
+                if todo_item['status'] != 'ACTIVE' : continue
+                # remove not having any OTP
+                if todo_item['otpDetails'] == None : continue
+                # remove OTP not on app Time4ID 
+                if todo_item['otpDetails']['otpMode'] != 1 : continue
+                # now concatenate only relevant elements
+                c = dict((k, todo_item[k]) for k in ('alias','domain','label'))
+                c['otpId'] = todo_item['otpDetails']['otpId'] 
+                c['otpProvider'] = todo_item['otpDetails']['otpProvider'] 
+                # 0=sms, 1=app, 2=email
+                c['otpMode'] = todo_item['otpDetails']['otpMode'] 
+                result += [c]
         return result
 
 class Time4IdRPC(Time4MindRPC):
     'class for all Time4User RPC call'
+
     def getTokenInfo(self,otpId,otpProvider):
-        method = "getTokenInfo"
-        params = {"otpId": otpId, 'otpProvider': otpProvider }
-        return self.call(method, params)
-    def submitOOB(self,otpId,otpProvider,params):
-        method = "submitOOB"
-        return self.call(method, params)
+        params = {'otpId': otpId, 'otpProvider': otpProvider }
+        return self.call('getTokenInfo', params)
+
     def authorizeMobile(self,otpId,otpProvider,title,sender, message,
-        opType=0,authType=0,param=None):
+        opType=0,authType=0,wizard=None):
         # opType: 0=internal, 1=external, 2=signature 
         # authType: 0=none, 1=content, 2=result, 3=content+result
         content = '<?xml version=\"1.0\" encoding=\"UTF-8\"?> <?xml-stylesheet type=\"text/xml\" href=\"#stylesheet\"?> <!DOCTYPE catalog [<!ATTLIST xsl:stylesheet id ID #REQUIRED> ]> <catalog> <xsl:stylesheet id=\"stylesheet\" version=\"1.0\" xmlns:xsl=\"http://www.w3.org/1999/XSL/Transform\"> <xsl:template match=\"/\"> <html> <body> ' + message + ' </body> </html> </xsl:template> </xsl:stylesheet> </catalog>'
-        params = {"otpId": otpId, "otpProvider": otpProvider, 
-            "header": { "title": title, "sender": sender }, 
-            "data": { "contentType": "application/xml", "content": content }, 
-            "opType": opType, "authType": authType }
+        params = {'otpId': otpId, 'otpProvider': otpProvider, 
+            'header': { 'title': title, 'sender': sender }, 
+            'data': { 'contentType': 'application/xml', 'content': content }, 
+            'opType': opType, 'authType': authType }
         if self.restHook: params['restHook'] = self.restHook
-        if param: params['param'] = param
-        return self.submitOOB(otpId, otpProvider, params)
-    def signMobile(self,otpId,otpProvider,title,sender, message,label,restHook=None):
-        param = [ { 
-            "tag": "Sign PIN",
-            "name": "Certificate PIN",
-            "description": "Insert your PIN to sign with " + label,
-            "type": "PIN"
+        if wizard: params['param'] = wizard
+        return self.call('submitOOB', params)
+
+    def signMobile(self,otpId,otpProvider,title,sender,message,label,restHook=None):
+        wizard = [ { 
+            'tag': 'Sign PIN',
+            'name': 'Certificate PIN',
+            'description': 'Insert your PIN to sign with ' + label,
+            'type': 'PIN'
             } ]
         return self.authorizeMobile(otpId,otpProvider,title,sender, message,
-            restHook=restHook,opType=2,authType=3,param=param)
+            opType=2,authType=3,wizard=wizard)
+
 
